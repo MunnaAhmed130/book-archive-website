7c21a8f74371c79ca0d0ad39aa2fa6b869ef4684
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-09-02 14:43:45 +0600

    commit

--- book-archive.js
+++ book-archive.js
@@ -3,6 +3,9 @@ const searchBooks = () => {
     const searchText = searchField.value;
     const emptySearch = document.getElementById('empty-search');
     searchField.value = '';
+    const cardContainer = document.getElementById("card-container");
+    const spinner = document.getElementById('spinner');
+    cardContainer.textContent = '';
     if (!searchText) {
         console.log('no result');
         emptySearch.style.display = ('block');
@@ -11,25 +14,34 @@ const searchBooks = () => {
         const spinner = document.getElementById('spinner');
         spinner.style.display=('block')
         emptySearch.style.display = ('none');
+        const bookCount = document.getElementById('book-count');
+        bookCount.innerText = '';
         fetch(`http://openlibrary.org/search.json?q=${searchText}`)
             .then(res => res.json())
-            .then(data => displayBooks(data.docs))
+            .then(data => displayBooks(data))
     }
 
 }
-
 const displayBooks = (data) => {
+    console.log(data)
+    const numFound = data.numFound;
+    const bookCount = document.getElementById('book-count');
+    bookCount.innerText = '';
+    bookCount.innerText=`Books Found: ${numFound}`
+    console.log(numFound);
     const cardContainer = document.getElementById("card-container");
-    cardContainer.textContent = '';
     const spinner = document.getElementById('spinner');
-    spinner.style.display=('none')
-    data.forEach(book => {
+    cardContainer.textContent = '';
+    spinner.style.display = ('none')
+
+    data.docs.forEach(book => {
         console.log(book);
+
+        spinner.style.display = ('none')
         const firstPublishYear = book.first_publish_year;
         const cover_i = book.cover_i;
         const title = book.title;
-        const authorName = book.author_name
-        
+        const authorName = book.author_name;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
