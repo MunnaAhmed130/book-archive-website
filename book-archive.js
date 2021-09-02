c3b745c06b951c2c6f08d194999b6e6b49851cf3
MunnaAhmed130 <munnaahmed2025@gmail.com>
2021-09-02 16:18:02 +0600

    commit

--- book-archive.js
+++ book-archive.js
@@ -8,6 +8,7 @@ const searchBooks = () => {
     cardContainer.textContent = '';
     if (!searchText) {
         console.log('no result');
+        emptySearch.innerText='NO RESULT'
         emptySearch.style.display = ('block');
     }
     else {
@@ -16,7 +17,7 @@ const searchBooks = () => {
         emptySearch.style.display = ('none');
         const bookCount = document.getElementById('book-count');
         bookCount.innerText = '';
-        fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+        fetch(`https://openlibrary.org/search.json?q=${searchText}`)
             .then(res => res.json())
             .then(data => displayBooks(data))
     }
@@ -24,36 +25,44 @@ const searchBooks = () => {
 }
 const displayBooks = (data) => {
     console.log(data)
+    const emptySearch = document.getElementById('empty-search');
     const numFound = data.numFound;
     const bookCount = document.getElementById('book-count');
     bookCount.innerText = '';
-    bookCount.innerText=`Books Found: ${numFound}`
+
     console.log(numFound);
     const cardContainer = document.getElementById("card-container");
     const spinner = document.getElementById('spinner');
     cardContainer.textContent = '';
     spinner.style.display = ('none')
-
-    data.docs.forEach(book => {
-        console.log(book);
-
-        spinner.style.display = ('none')
-        const firstPublishYear = book.first_publish_year;
-        const cover_i = book.cover_i;
-        const title = book.title;
-        const authorName = book.author_name;
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-100">
-            <img src=" https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top" height="300px" alt="...">
-            <div class="card-body">
-                <h4 class="card-title">${title}</h4>
-                <h6 class="card-title">Author/Author's: <br>${authorName}</h6>
-                <h6>First Published in : ${firstPublishYear}</h6>
+    if (!numFound) {
+        console.log('No Match Found');
+        emptySearch.innerText='NO MATCH FOUND'
+        emptySearch.style.display = ('block');
+    }
+    else {
+        data.docs.forEach(book => {
+            console.log(book);
+            bookCount.innerText=`Books Found: ${numFound}`
+            spinner.style.display = ('none')
+            const firstPublishYear = book.first_publish_year;
+            const cover_i = book.cover_i;
+            const title = book.title;
+            const authorName = book.author_name;
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100">
+                <img src=" https://covers.openlibrary.org/b/id/${cover_i}-M.jpg" class="card-img-top" height="300px" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${title}</h4>
+                    <h6 class="card-title">Author/Author's: <br>${authorName}</h6>
+                    <h6>First Published in : ${firstPublishYear}</h6>
+                </div>
             </div>
-        </div>
-        `;
-        cardContainer.appendChild(div)
-    });
+            `;
+            cardContainer.appendChild(div)
+        });
+    }
+
 }
